ac7624ba2f6d071e62c3b6b8498a91bb11d35ff6
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:30:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orkin-pestidentifie-45939",
+  url: "https://orkin-pestidentifie-45939.botics.co",
+  api: "https://orkin-pestidentifie-45939.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
